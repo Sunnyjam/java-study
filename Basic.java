83a73bd0b607678e2a3ba9c5ddba0d9069c5fd3f
Sunnyjam <gustjs0540@naver.com>
2022-09-25 17:00:11 +0900

    test

--- Basic.java
+++ Basic.java
@@ -1,6 +1,43 @@
+/*
+* 사칙연산
+* */
 public class Basic {
 
+     // Type
+     // 정수 : int
+    // 실수 : double, float
     public static void main(String[] args) {
-        System.out.println("Git First Push");
+        // 1 + 1 = 2
+        // 선언
+        int number = 1;
+        int number2 = 1;
+
+        // 출력
+        System.out.println(number + number2);
+
+        // 1 - 1 = 0
+        // 선언
+        int number3 = 1;
+        int number4 = 1;
+
+        // 출력
+        System.out.println(number3 - number4);
+
+        // 1 x 1 = 1
+        // 선언
+        int number5 = 1;
+        int number6 = 1;
+
+        // 출력
+        System.out.println(number5 * number6);
+        // 1 / 1 = 1
+        // 선언
+        int number7 = 1;
+        int number8 = 1;
+
+        // 출력
+        System.out.println(number7 / number8);
+        System.out.println(number7 % number8);
     }
+
 }
