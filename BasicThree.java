498ab81416a1acbde6716b356d92fddb1d1d400b
Sunnyjam <gustjs0540@naver.com>
2022-09-25 17:30:43 +0900

    test 사칙연산 개선 사칙연산 문제만들기

---
+++ BasicThree.java
@@ -0,0 +1,12 @@
+public class BasicThree {
+    public static void main(String[] args) {
+        // 실수
+        // double, float
+
+        // 1.0 + 1.0 = 2.0
+        double number = 1.0;
+        double number2 = 1.0;
+
+        System.out.println( number + number2 );
+    }
+}
