00fb9827781225301ae76a758db49bfc0ff3b6f8
Sunnyjam <gustjs0540@naver.com>
2022-10-10 14:10:02 +0900

    20221010

---
+++ ifelse/ElseIf.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class ElseIf {
+    public static void main(String[] args) {
+        // else if 조건 추가하기.
+        int money = 5000;
+        if(money >= 8000){
+            System.out.println("크로플 배달시키기.");
+        }else if (money >= 5000){
+            System.out.println("빵집 배달시키기.");
+        } else {
+            System.out.println("방문 포장.");
+        }
+
+    }
+}
